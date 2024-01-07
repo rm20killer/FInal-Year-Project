64906e1f57a1427c44e3557e940d49c9da172211
RM20 <43253883+rm20killer@users.noreply.github.com>
2024-01-07 00:49:49 +0000

    working on Kuwahara Filter

---
+++ UnityProject/FinalYearProject/Assets/RenderPass/Kuwahara/KuwaharaFeature.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class KuwaharaFeature : ScriptableRendererFeature
+{
+    Material compositeMaterial;
+    [System.Serializable]
+    public class MyFeatureSettings
+    {
+        public bool IsEnabled = true;
+        public RenderPassEvent WhenToInsert = RenderPassEvent.AfterRendering;
+        public Shader shader;
+    }
+    
+    public MyFeatureSettings settings = new MyFeatureSettings();
+
+    RTHandle renderTextureHandle;
+    ImpactEffectRenderPass impactEffectRenderPass;
+
+    public override void Create()
+    {
+        compositeMaterial = CoreUtils.CreateEngineMaterial(settings.shader);
+        impactEffectRenderPass = new ImpactEffectRenderPass(
+            "ImpactEffectRenderPass",
+            settings.WhenToInsert,
+            compositeMaterial
+        );
+    }
+    
+    public override void SetupRenderPasses(ScriptableRenderer renderer, in RenderingData renderingData) {
+        impactEffectRenderPass.Setup(renderer.cameraColorTargetHandle);
+
+    }
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        if (!settings.IsEnabled)
+        {
+            return;
+        }
+        renderer.EnqueuePass(impactEffectRenderPass);
+        
+        renderer.EnqueuePass(impactEffectRenderPass);
+    }
+}
---
+++ UnityProject/FinalYearProject/Assets/RenderPass/Kuwahara/KuwaharaPass.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class KuwaharaPass : ScriptableRenderPass
+{
+    string profilerTag;
+
+    Material CompositeMaterial;
+    RTHandle cameraColorTargetIdent;
+    RenderTargetHandle tempTexture;
+
+    public ImpactEffectRenderPass(string profilerTag,
+        RenderPassEvent renderPassEvent, Material compositeMaterial)
+    {
+        this.profilerTag = profilerTag;
+        this.renderPassEvent = renderPassEvent;
+        this.CompositeMaterial = compositeMaterial;
+    }
+
+
+    public void Setup(RTHandle cameraColorTargetIdent)
+    {
+        this.cameraColorTargetIdent = cameraColorTargetIdent;
+    }
+  
+    public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+    {
+        cmd.GetTemporaryRT(tempTexture.id, cameraTextureDescriptor);
+    }
+
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+    {
+        CommandBuffer cmd = CommandBufferPool.Get(profilerTag);
+        using (new ProfilingSample(cmd, profilerTag))
+        {
+            Render(cmd, ref renderingData);
+        }
+        context.ExecuteCommandBuffer(cmd);
+        cmd.Clear();
+    
+        CommandBufferPool.Release(cmd);
+    }
+
+    void Render(CommandBuffer cmd, ref RenderingData renderingData)
+    {
+        cmd.Blit(cameraColorTargetIdent, tempTexture.Identifier(), CompositeMaterial, 0);
+        cmd.Blit(tempTexture.Identifier(), cameraColorTargetIdent);
+    }
+
+    public override void FrameCleanup(CommandBuffer cmd)
+    {
+        cmd.ReleaseTemporaryRT(tempTexture.id);
+    }
+}
